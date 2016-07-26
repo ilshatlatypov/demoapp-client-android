581827bf668b9b05f14d0b077e902e3987fb150c
ilshat <latypov.ilshat@gmail.com>
2016-07-26 22:03:04 +0300

    add splash activity

---
+++ app/src/main/java/org/hello/SplashActivity.java
@@ -0,0 +1,20 @@
+package org.hello;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ilshat on 26.07.16.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, PersonsListActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
