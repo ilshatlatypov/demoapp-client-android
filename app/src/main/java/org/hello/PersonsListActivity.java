d0f015a16d2418ce8552133029b222989a10fc17
ilshat <latypov.ilshat@gmail.com>
2016-07-28 20:58:52 +0300

    rename layout xml files for persons list activity

--- app/src/main/java/org/hello/PersonsListActivity.java
+++ app/src/main/java/org/hello/PersonsListActivity.java
@@ -38,7 +38,7 @@ public class PersonsListActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_persons_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
