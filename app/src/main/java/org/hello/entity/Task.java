a5ae17a61a0bdfe6332d2fa67c8bbea5c383d8ee
ilshat <latypov.ilshat@gmail.com>
2016-08-04 21:10:14 +0300

    add task class and corresponding navigation menu option

---
+++ app/src/main/java/org/hello/entity/Task.java
@@ -0,0 +1,29 @@
+package org.hello.entity;
+
+/**
+ * Created by ilshat on 04.08.16.
+ */
+public class Task {
+
+    private String self;
+    private String title;
+
+    public Task() {
+    }
+
+    public String getSelf() {
+        return self;
+    }
+
+    public void setSelf(String self) {
+        this.self = self;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
